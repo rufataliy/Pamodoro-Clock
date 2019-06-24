c24fb710236ec570689d4c350a1724ad3944a72a
Rufat Aliyev <rufataliyevbakou@gmail.com>
2019-06-24 17:50:04 -0400

    Updates

---
+++ precache-manifest.82432bb8718c2dc691dba6e5d93e4ca7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7a82770dd9cae202ff2f8c58e53d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "3d18b2552a789ed05538",
+    "url": "/static/js/main.43ac38b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
